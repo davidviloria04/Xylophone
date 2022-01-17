58af38604ff83d16cee9927063ff8051844327c8
David Viloria <david.viloria0424@gmail.com>
2022-01-16 21:56:59 -0500

    Se agrega el sonido al Botón C

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -8,6 +8,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playAudioFromProject()
+    }
+    
+    func playAudioFromProject() {
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else {
+            print("error to get the mp3 file")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+        } catch {
+            print("audio file error")
+        }
+    }
 }
 
 
